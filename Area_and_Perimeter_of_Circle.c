fbcdfcc2aafd249403e3d2bfd7a0e7d10a856a3e
23MHCQ05254 <147487928+23MHCQ05254@users.noreply.github.com>
2023-10-16 15:28:07 +0530

    #include<stdio.h> int main()
    {
        float r;
        //loat pi=3.14;
        float per,area;
        scanf("%f",&r);
        area=3.14*r*r;
        per=2*3.14*r;
        printf ("%.2f
    %.2f",area,per);
        
        
    }

---
+++ Area_and_Perimeter_of_Circle.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+
+{
+    float r;
+    //loat pi=3.14;
+    float per,area;
+    scanf("%f",&r);
+    area=3.14*r*r;
+    per=2*3.14*r;
+    printf ("%.2f
+%.2f",area,per);
+    
+    
+}
