337edc11998ac9345f038dce3f550e9a51ff5fe0
23MHCQ05254 <147487928+23MHCQ05254@users.noreply.github.com>
2023-10-26 12:26:43 +0530

    #include<stdio.h> int main() {     int ir,cir;     scanf("%d%d",&ir,&cir);     if(ir>=2*cir)     {         printf("YES");
       }
        else
        {
            printf("NO");
        }
       
    }

---
+++ Good_Investment__or__Not.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int ir,cir;
+    scanf("%d%d",&ir,&cir);
+    if(ir>=2*cir)
+    {
+        printf("YES");
+        
+   }
+    else
+    {
+        printf("NO");
+    }
+   
+}
