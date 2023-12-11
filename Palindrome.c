e6469d4b45dffde9cf81e1a1412f45597f065f00
23MHCQ05254 <147487928+23MHCQ05254@users.noreply.github.com>
2023-12-11 14:33:02 +0530

    #include<stdio.h> int main() {     int n,re=0,o;     scanf("%d",&n);     o=n;     while(n!=0)     {         int r=n%10;         re=re*10+r;         n=n/10;     }     if(o==re)     printf("True");     else     printf("False");
    }

--- Palindrome.c
+++ Palindrome.c
@@ -1,41 +1,18 @@
 #include<stdio.h>
 int main()
 {
-  /*  int n,re=0;
-    scanf("%d",&n);
-    while(n!=0)
-    {
-        int r=n%10;
-        int re=re*10+r;
-        n=n/10;
-    }
-    if(re==n)
-    {
-        printf("Palindrome");
-    }
-    else
-    {
-        printf("Not Palindrome");
-    }
-}
-include<stdio.h>
-int main()*/
-
-    int n,re=0,r,k,p,o;
+    int n,re=0,o;
     scanf("%d",&n);
     o=n;
     while(n!=0)
     {
-        r=n%10;
+        int r=n%10;
         re=re*10+r;
         n=n/10;
     }
-    
     if(o==re)
-    printf("Palindrome");
+    printf("True");
     else
-    printf("Not Palindrome");
-    
-    
+    printf("False");
     
 }
