ee6837193bc1715aca2829918e69e8e8f634e2e8
23MHCQ05254 <147487928+23MHCQ05254@users.noreply.github.com>
2023-12-22 20:59:59 +0530

    #include<stdio.h> int main() {     int n,i,j,c;     scanf("%d",&n);     for(int i=1;i<=n;i++)     {     	for(j=1;j<=n;j++)     	{     		printf("%d ",j); 		} 		printf(" "); 	} }

---
+++ Basic_Pattern_1.2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,c;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+    	for(j=1;j<=n;j++)
+    	{
+    		printf("%d ",j);
+		}
+		printf("
+");    	
+	}
+}
+    	
