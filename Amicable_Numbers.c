19d94e853cd1f3c5324ac4ef9cb1ebf5a494d126
23MHCQ05254 <147487928+23MHCQ05254@users.noreply.github.com>
2024-01-03 09:40:36 +0530

    #include<stdio.h> int main() { 	int i,a,b,sum=0,mus=0; 	scanf("%d%d",&a,&b); 	for(i=1;i<a;i++) 	{ 		if(a%i==0) 		{ 			sum=sum+i; 		} 	} 	for(i=1;i<b;i++) 	{ 		if(b%i==0) 		{ 			mus=mus+i; 		} 	} 	if(mus==a && sum==b) 	{ 		printf("Amicable"); 	} 	else 	{ 		printf("Not Amicable"); 	} }

---
+++ Amicable_Numbers.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+	int i,a,b,sum=0,mus=0;
+	scanf("%d%d",&a,&b);
+	for(i=1;i<a;i++)
+	{
+		if(a%i==0)
+		{
+			sum=sum+i;
+		}
+	}
+	for(i=1;i<b;i++)
+	{
+		if(b%i==0)
+		{
+			mus=mus+i;
+		}
+	}
+	if(mus==a && sum==b)
+	{
+		printf("Amicable");
+	}
+	else
+	{
+		printf("Not Amicable");
+	}
+}
